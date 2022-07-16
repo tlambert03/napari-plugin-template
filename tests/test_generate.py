2ebd08f37d5c1003e7325b9be017d9f78a907d6a
Talley Lambert <talley.lambert@gmail.com>
2022-07-16 18:07:22 -0400

    fix many things, test tests

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -3,7 +3,6 @@ import shutil
 import tempfile
 from contextlib import contextmanager
 from pathlib import Path
-from re import TEMPLATE
 from subprocess import run
 from typing import Any, Callable
 
@@ -11,6 +10,16 @@ import pytest
 import tomli
 
 TEMPLATE = Path(__file__).parent.parent
+
+
+@contextmanager
+def run_copier(template: Path, **kwargs: Any):
+    with tempfile.TemporaryDirectory() as td:
+        cmd = ["copier", "--force", str(template), str(td)]
+        for k, v in kwargs.items():
+            cmd.extend(["-d", f"{k}={v!r}"])
+        run(cmd, check=True)
+        yield Path(td)
 
 
 @contextmanager
@@ -37,55 +46,45 @@ def template():
         yield Path(td)
 
 
-@pytest.fixture
-def copier(tmp_path: Path):
-    def _copier(template: Path, dest: Path = tmp_path, **kwargs: Any):
-        cmd = ["copier", "--force", str(template), str(dest)]
-        for k, v in kwargs.items():
-            cmd.extend(["-d", f"{k}={v!r}"])
-        run(cmd, check=True)
-        return tmp_path
-
-    return _copier
-
-
-def test_copier(template: Path, copier: Callable[..., Path]):
+@pytest.fixture(scope="session")
+def built(template):
     NAME = "some-project"
-    output = copier(
+    with run_copier(
+        template, plugin_name=NAME, full_name="Test Name", email="test@example.com"
+    ) as out:
+        with inside_dir(str(out)):
+            run(["git", "init", "-q"], check=True)
+            gitcfg = out / ".git" / "config"
+            gitcfg.touch()
+            gitcfg.write_text("[user]\n\tname = Name\n\temail = email@wp.p\n")
+            run(["git", "add", "."], check=True)
+            run(["git", "commit", "-q", "-m", "init"], check=True)
+            yield out
+
+
+def test_copier(template: Path):
+    NAME = "some-project"
+    with run_copier(
         template, full_name="Test Name", email="test@example.com", plugin_name=NAME
-    )
-    prj = tomli.loads((output / "pyproject.toml").read_text())["project"]
-    assert prj["name"] == NAME
-    assert prj["authors"] == [{"email": "test@example.com", "name": "Test Name"}]
+    ) as out:
+        prj = tomli.loads((out / "pyproject.toml").read_text())["project"]
+        assert prj["name"] == NAME
+        assert prj["authors"] == [{"email": "test@example.com", "name": "Test Name"}]
 
 
-# def test_bake_and_test(template: Path, copier: Callable[..., Path]):
-#     NAME = "some-project"
-#     output = copier(template, plugin_name=NAME)
-#     run(["python", "-m", "pytest", str(output / "src" / "some_project")], check=True)
+def test_check_manifest(built):
+    run(["check-manifest"], check=True)
 
 
-def test_bake_and_build(template, copier: Callable[..., Path]):
-    output = copier(template, full_name="Test Name", email="test@example.com")
-
-    with inside_dir(str(output)):
-        run(["git", "init", "-q"], check=True)
-        gitcfg = output / ".git" / "config"
-        gitcfg.touch()
-        gitcfg.write_text("[user]\n\tname = Name\n\temail = email@wp.p\n")
-        run(["git", "add", "."], check=True)
-        run(["git", "commit", "-q", "-m", "init"], check=True)
-        run(["check-manifest"], check=True)
-        run(["python", "-m", "build"], check=True)
-        assert len(list((output / "dist").iterdir())) == 2
+def test_build(built):
+    run(["python", "-m", "build"], check=True)
+    assert len(list((built / "dist").iterdir())) == 2
 
 
-def test_bake_and_pre_commit(template, copier: Callable[..., Path]):
-    output = copier(template)
+def test_bake_and_pre_commit(built):
+    run(["pre-commit", "autoupdate"], check=True)
+    run(["pre-commit", "run", "--all-files"], check=True)
 
-    with inside_dir(str(output)):
-        run(["git", "init", "-q"], check=True)
-        run(["pre-commit", "autoupdate"], check=True)
-        run(["pre-commit", "install"], check=True)
-        run(["git", "add", "."], check=True)
-        run(["pre-commit", "run", "--all-files"], check=True)
+
+def test_bake_and_test(built):
+    run(["tox", "--verbose"], check=True)
